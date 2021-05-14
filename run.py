3911f6910bcee9d2a6eeba3b9af07be943708f7f
niall-mulcahy <niall-mulcahy@outlook.com>
2021-05-14 12:31:52 +0000

    have templating working properly, added register page, and dashboard

--- run.py
+++ run.py
@@ -7,7 +7,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("base.html")
+    return render_template("index.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
 
 
 if __name__ == "__main__":
